20086704c2755a87dea4c8382aa0f6f6026cb6f1
Aman kumar <72993406+AMANJIKUMAR@users.noreply.github.com>
2022-03-05 22:18:02 +0530

    Add files via upload

---
+++ 12_even_for_loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    int i,n;
+    cin>>n;
+
+    for(i=1;i<=n;i=i+1){
+        if(i%2 == 0){
+            cout<<i<<' ';
+        }
+    }
+    cout<<endl;
+    return 0;
+}
